52f247798eaefe055867229bc736e9daeafc3e50
XeniaKlimenko <xeniaklim2000@yandex.ru>
2020-12-17 00:15:56 +0300

    First version

---
+++ Polinom/Polinom.h
@@ -0,0 +1,26 @@
+#ifndef _POLINOMIAL_
+#define _POLINOMIAL_
+
+#include <iostream>
+#include <fstream>
+#include "Monom.h"
+#include "List.h"
+
+
+using namespace std;
+
+class TPolinomial : public TList<TMonomData>
+{
+public:
+  TPolinomial();
+  TPolinomial(TPolinomial& _v);
+  ~TPolinomial();
+
+  TPolinomial& operator+= (TMonom& _v);
+
+  TPolinomial operator* (TPolinomial& _v);
+  TPolinomial operator+ (TPolinomial& _v);
+  TPolinomial operator- (TPolinomial& _v);
+
+};
+#endif
